eae5394708dd2d7813c327dacfb4c9da67d5fbb9
Nobat <nobatgeldi@covisart.com>
2019-06-22 16:01:03 +0300

    tutoraila 0.0.0.1

---
+++ Summer2019dev_murat/Assets/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public GameObject cube;
+    public int size;
+    public void changePosition()
+    {
+        cube.transform.localPosition = new Vector3(0, size, 0);
+    }
+
+    void FixedUpdate()
+    {
+        cube.transform.localPosition = new Vector3(0, 0, size);
+    }
+}
