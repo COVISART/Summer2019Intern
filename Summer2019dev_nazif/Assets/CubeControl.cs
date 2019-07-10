7f4c3ee5bd4c27d7409ff9afdfc27dc12e394854
Nazif Furkan <naziffurkandemirsoy@gmail.com>
2019-07-10 15:58:37 +0300

    egitim1

---
+++ Summer2019dev_nazif/Assets/CubeControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    public Transform currentPos;
+    public float speed;
+    public void changePosition(Transform targetPos)
+    {
+        currentPos = targetPos;
+
+    }
+
+    void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetObject.transform.localPosition = Vector3.Lerp(transform.localPosition, currentPos.localPosition, speed);
+        targetObject.transform.rotation = Quaternion.Lerp(targetObject.transform.rotation,currentPos.rotation,speed);
+        targetObject.transform.rotation = Quaternion.Lerp(targetObject.transform.rotation,currentPos.rotation,speed);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
