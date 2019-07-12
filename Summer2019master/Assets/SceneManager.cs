d99db53ab5aa980104bded51f48b2b22555a53b0
Nobat <nobatgeldi@covisart.com>
2019-07-12 20:17:40 +0300

    egitim 2

---
+++ Summer2019master/Assets/SceneManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void CloseApp()
+    {
+        Application.Quit();
+        Debug.Log("CloseApp Trigered");
+    }
+
+    public void LoadLevel(int scene)
+    {
+        StartCoroutine(LoadYourAsyncScene(scene));
+    }
+
+    IEnumerator LoadYourAsyncScene(int scene)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        var asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(scene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+
+            yield return null;
+        }
+    }
+}
